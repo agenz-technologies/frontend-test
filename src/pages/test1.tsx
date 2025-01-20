570ab862100b21c8f14ccba5aaeb1647eabdc5c0
Badr Belkeziz <b.belkeziz@gmail.com>
2025-01-20 15:32:16 +0100

    Explanations

--- src/pages/test1.tsx
+++ src/pages/test1.tsx
@@ -7,6 +7,15 @@ function HiringTest1() {
   };
   return (
     <>
+      <h1>
+        In the production environnement, this page is accessible to all
+        visitors. Its a static page and all visitors are served with the same
+        content
+      </h1>
+      <h2>
+        The click id will be used to track users interaction with the page
+        during the visit
+      </h2>
       <button type="button" onClick={handleClick}>
         Click Me !
       </button>
